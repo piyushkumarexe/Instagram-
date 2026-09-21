a48decc0fa77a2a3040c17238996a35d69e9aded
piyushkumarexe <8.9646078e+07+piyushkumarexe@users.noreply.github.com>
2026-09-21 05:04:23 +0000

    Match mobile nav icons to Instagram screenshot

--- client/src/components/MobileNav.jsx
+++ client/src/components/MobileNav.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { NavLink, useNavigate, useLocation } from 'react-router-dom'
 import { useApp } from '../store.jsx'
 import Avatar from './Avatar.jsx'
-import { IcHome, IcHomeFill, IcSearch, IcSearchFill, IcReels, IcReelsFill, IcPlusSquare, IcHeart, IcHeartFill, IcSend } from './Icons.jsx'
+import { IcHome, IcHomeFill, IcSearch, IcSearchFill, IcReels, IcReelsFill, IcHeart, IcSend } from './Icons.jsx'
 
 export default function MobileNav() {
   const app = useApp()
@@ -15,11 +15,24 @@ export default function MobileNav() {
 
   return (
     <nav className="mobile-nav">
-      <NavLink to="/" end className="mnav-item">{({ isActive }) => isActive ? <IcHomeFill size={26} /> : <IcHome size={26} />}</NavLink>
-      <NavLink to="/explore" className="mnav-item">{({ isActive }) => isActive ? <IcSearchFill size={25} /> : <IcSearch size={25} />}</NavLink>
-      <button className="mnav-item" onClick={() => app.openCreate('post')}><IcPlusSquare size={26} /></button>
-      <NavLink to="/reels" className="mnav-item">{({ isActive }) => isActive ? <IcReelsFill size={26} /> : <IcReels size={26} />}</NavLink>
-      <button className="mnav-item mnav-profile" onClick={() => nav('/' + app.user.username)}>
+      <NavLink to="/" end className="mnav-item">
+        {({ isActive }) => isActive ? <IcHomeFill size={26} /> : <IcHome size={26} />}
+      </NavLink>
+      <NavLink to="/reels" className="mnav-item">
+        {({ isActive }) => isActive ? <IcReelsFill size={26} /> : <IcReels size={26} />}
+      </NavLink>
+      <NavLink to="/messages" className="mnav-item mnav-messages" style={{ position: 'relative' }}>
+        <IcSend size={26} />
+        {!!app.unreadDMs && (
+          <span className="nav-badge" aria-label={`${app.unreadDMs} unread messages`}>
+            {app.unreadDMs > 9 ? '9+' : app.unreadDMs}
+          </span>
+        )}
+      </NavLink>
+      <NavLink to="/explore" className="mnav-item">
+        {({ isActive }) => isActive ? <IcSearchFill size={25} /> : <IcSearch size={25} />}
+      </NavLink>
+      <button className="mnav-item mnav-profile" onClick={() => nav('/' + app.user.username)} aria-label="Profile">
         <Avatar user={app.user} size={27} />
       </button>
     </nav>
